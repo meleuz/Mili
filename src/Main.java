23f066371af8a5a7e549bfb73b48a36e7d6e1d00
Aigul Biktasheva <meleuz13@gmail.com>
2021-03-11 21:29:05 +0500

    milesNumber added

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        double ticketSellKop = 125_000_000.50;
         int ticketSellKop = 12500_50;
 
         if (ticketSellKop < 0) {
             ticketSellKop = 0;
         }
 
+        double milesNumber = ticketSellKop / 20;
+
+        System.out.printf("%.2f", milesNumber);
         int milesNumber = ticketSellKop / 20 / 100;
         System.out.println(milesNumber);
     }
